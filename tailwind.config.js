0d1ce66211f8dc0dd472456d706dd4681ae6c01b
Stanley Payne <stanleyjpayne@gmail.com>
2025-04-28 14:43:43 -0400

    Replace tailwind files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+      },
+    },
+  },
+  plugins: [],
+};
